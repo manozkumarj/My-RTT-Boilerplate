cdea384c68a5d8d420a862b7c28c7af2f6ceb63d
Manoz Mac <manojmani517@gmail.com>
2026-05-21 23:04:11 +0530

    Add redux-logger for enhanced Redux state logging

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit"
+import { createLogger } from "redux-logger"
 import todosReducer from "./slices/todosSlice"
 import counterReducer from "./slices/counterSlice"
+
+const logger = createLogger();
 
 const store = configureStore({
     reducer: {
         todos: todosReducer,
         counter: counterReducer
     },
+    middleware: (getDefaultMiddleware) => [
+        ...getDefaultMiddleware({
+            serializableCheck: false
+        }),
+        logger
+    ],
+    devTools: false
 })
 
 export default store
